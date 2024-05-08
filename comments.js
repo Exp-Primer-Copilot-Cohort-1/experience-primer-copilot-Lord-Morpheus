e49f247b189c79b936921c7d59cb352887c23d45
Tarun Srivastava <tarurak4@gmail.com>
2024-05-08 06:10:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,63 @@
 // create web server
-// var http = require('http');
-// var server = http.createServer(function(req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/plain' });
-//     res.end('Hello World\n');
-// });
-// server.listen(3000, 'done');
+const http = require('http');
+const fs = require('fs');
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.end('Hello World')
+});
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+});
+// read file
+fs.readFile('comments.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
+});
+// write file
+const comments = {
+  "comments": [
+    {
+      "name": "John",
+      "message": "Hello"
+    },
+    {
+      "name": "Jane",
+      "message": "Hi"
+    }
+  ]
+}
+fs.writeFile('comments.json', JSON.stringify(comments, null, 2), 'utf8', (err) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log('File has been written')
+});
+// update file
+fs.readFile('comments.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  const comments = JSON.parse(data)
+  comments.comments.push({ "name": "Tom", "message": "Hey" })
+  fs.writeFile('comments.json', JSON.stringify(comments, null, 2), 'utf8', (err) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log('File has been updated')
+  });
+});
+// delete file
+fs.unlink('comments.json', (err) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log('File has been deleted')
+});
+// Path: index.js
