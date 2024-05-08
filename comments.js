b1bec3ce11272e4bfef62ec9e4bd96387ab0db63
Tarun Srivastava <tarurak4@gmail.com>
2024-05-08 06:17:12 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,63 +1,74 @@
-// create web server
-const http = require('http');
-const fs = require('fs');
+//Create web server
+const express = require('express');
+const app = express();
 const port = 3000;
-const server = http.createServer((req, res) => {
-  res.end('Hello World')
+
+//Create a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
+
+//Start the server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-// read file
-fs.readFile('comments.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  console.log(data)
+
+// Path: comments.js
+//Create a route to display comments
+app.get('/comments', (req, res) => {
+  res.send('Comments will be displayed here');
 });
-// write file
-const comments = {
-  "comments": [
-    {
-      "name": "John",
-      "message": "Hello"
-    },
-    {
-      "name": "Jane",
-      "message": "Hi"
-    }
-  ]
-}
-fs.writeFile('comments.json', JSON.stringify(comments, null, 2), 'utf8', (err) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  console.log('File has been written')
+
+// Path: comments.js
+//Create a route to display a single comment
+app.get('/comments/:id', (req, res) => {
+  res.send('Comment with id ' + req.params.id + ' will be displayed here');
 });
-// update file
-fs.readFile('comments.json', 'utf8', (err, data) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  const comments = JSON.parse(data)
-  comments.comments.push({ "name": "Tom", "message": "Hey" })
-  fs.writeFile('comments.json', JSON.stringify(comments, null, 2), 'utf8', (err) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    console.log('File has been updated')
-  });
+
+// Path: comments.js
+//Create a route to post a comment
+app.post('/comments', (req, res) => {
+  res.send('Comment has been posted');
 });
-// delete file
-fs.unlink('comments.json', (err) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  console.log('File has been deleted')
+
+// Path: comments.js
+//Create a route to delete a comment
+app.delete('/comments/:id', (req, res) => {
+  res.send('Comment with id ' + req.params.id + ' has been deleted');
 });
-// Path: index.js
+
+// Path: comments.js
+//Create a route to update a comment
+app.put('/comments/:id', (req, res) => {
+  res.send('Comment with id ' + req.params.id + ' has been updated');
+});
+
+// Path: comments.js
+//Create a route to upvote a comment
+app.put('/comments/:id/upvote', (req, res) => {
+  res.send('Comment with id ' + req.params.id + ' has been upvoted');
+});
+
+// Path: comments.js
+//Create a route to downvote a comment
+app.put('/comments/:id/downvote', (req, res) => {
+  res.send('Comment with id ' + req.params.id + ' has been downvoted');
+});
+
+// Path: comments.js
+//Create a route to flag a comment
+app.put('/comments/:id/flag', (req, res) => {
+  res.send('Comment with id ' + req.params.id + ' has been flagged');
+});
+
+// Path: comments.js
+//Create a route to unflag a comment
+app.put('/comments/:id/unflag', (req, res) => {
+  res.send('Comment with id ' + req.params.id + ' has been unflagged');
+});
+
+// Path: comments.js
+//Create a route to get all flagged comments
+app.get('/comments/flagged', (req, res) => {
+  res.send('All flagged comments will be displayed here');
+});
